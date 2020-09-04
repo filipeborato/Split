907a2b9bc12d776af852718e890f392e53cfc9ef
root <root@LAPTOP-NVEU241I.localdomain>
2020-09-03 21:16:18 -0300

    tentativa do Sleeter na Api

--- rest.py
+++ rest.py
@@ -3,14 +3,16 @@ from flask_restful import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
 import wave
 import numpy as np
+import split
 
-
+split.separa(2,"files/It dont mean i think_ master.mp3") 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
+    def get(self):        
         return {'hello': 'world'}
+            
 
 class UploadWavAPI(Resource):
     def post(self):
@@ -23,7 +25,8 @@ class UploadWavAPI(Resource):
         wav_file = wave.open(stream, 'rb')
         signal = wav_file.readframes(-1)
         signal = np.fromstring(signal, 'Int16')
-        fs = wav_file.getframerate()        
+        fs = wav_file.getframerate()
+        split.separa(4,wav_file)        
         wav_file.close()       
 
 api.add_resource(HelloWorld, '/')
