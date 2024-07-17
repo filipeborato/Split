70f5cd73bfc9db0f9d8bea7aaabe3a7bc1a6b82c
Your Name <you@example.com>
2024-07-17 17:07:03 +0200

    feat: handler error

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, send_from_directory, make_response
 from flask_restful import Resource, Api, reqparse
 from werkzeug.datastructures import FileStorage
+from werkzeug.exceptions import HTTPException
 from utils import split, ziped
 import os, json
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 @api.representation('application/json')
 def output_json(data, code, headers=None):
